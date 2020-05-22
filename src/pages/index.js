3489b96976b9318f7a6a1bc23c1dbc059f64f884
masayaShinoda <masayashida@gmail.com>
2020-05-22 19:47:34 +0700

    removed oversaturated portraits

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,6 @@ import Navbar from '../components/navbar'
 
 import { Link } from 'gatsby'
 
-
 //landscape imports
 import thumb01 from '../assets/images/thumbs/01.jpg'
 import thumb02 from '../assets/images/thumbs/02.jpg'
@@ -24,10 +23,6 @@ import full02 from '../assets/images/fulls/02.jpg'
 import full03 from '../assets/images/fulls/03.jpg'
 import full04 from '../assets/images/fulls/04.jpg'
 import full05 from '../assets/images/fulls/05.jpg'
-import full06 from '../assets/images/fulls/06.jpg'
-import full07 from '../assets/images/fulls/07.jpg'
-import full08 from '../assets/images/fulls/08.jpg'
-
 
 // portrait imports
 import thumbp01 from '../assets/images/thumbs/p01.jpg'
@@ -44,115 +39,182 @@ import fullp04 from '../assets/images/fulls/p04.jpg'
 import MyForm from '../components/myform'
 
 const DEFAULT_IMAGES = [
-    { id: '1', source: full01, thumbnail: thumb01, caption: 'Peace', description: 'Kamakura Big Buddha, Yokohama, Japan.'},
-    { id: '2', source: full02, thumbnail: thumb02, caption: 'Harmony', description: 'Kamakura Big Buddha, Yokohama, Japan.'},
-    { id: '3', source: full03, thumbnail: thumb03, caption: 'Tranquility', description: 'Kompot, Cambodia.'},
-    { id: '4', source: full04, thumbnail: thumb04, caption: 'Heroic', description: 'Kep, Cambodia.'},
-
-];
+  {
+    id: '1',
+    source: full01,
+    thumbnail: thumb01,
+    caption: 'Peace',
+    description: 'Kamakura Big Buddha, Yokohama, Japan.',
+  },
+  {
+    id: '2',
+    source: full02,
+    thumbnail: thumb02,
+    caption: 'Harmony',
+    description: 'Kamakura Big Buddha, Yokohama, Japan.',
+  },
+  {
+    id: '3',
+    source: full03,
+    thumbnail: thumb03,
+    caption: 'Tranquility',
+    description: 'Kompot, Cambodia.',
+  },
+  {
+    id: '4',
+    source: full04,
+    thumbnail: thumb04,
+    caption: 'Heroic',
+    description: 'Kep, Cambodia.',
+  },
+]
 
 const PORTRAIT_IMAGES = [
-    { id: 'p1', source: fullp01, thumbnail: thumbp01, caption: '', description: ''},
-    { id: 'p3', source: fullp03, thumbnail: thumbp03, caption: '', description: ''},
-];
+  {
+    id: 'p1',
+    source: fullp01,
+    thumbnail: thumbp01,
+    caption: '',
+    description: '',
+  },
+  {
+    id: 'p3',
+    source: fullp03,
+    thumbnail: thumbp03,
+    caption: '',
+    description: '',
+  },
+]
 
 class HomeIndex extends React.Component {
+  render() {
+    const siteTitle = 'Masaya Photography'
+    const siteDescription = 'A little photo album.'
 
-    render() {
-        const siteTitle = "Masaya Photography"
-        const siteDescription = "A little photo album."
+    return (
+      <Layout>
+        <Helmet>
+          <title>{siteTitle}</title>
+          <meta name="description" content={siteDescription} />
+          <meta name="theme-color" content="#4C88B3"></meta>
+        </Helmet>
 
-        return (
-            <Layout>
-                <Helmet>
-                        <title>{siteTitle}</title>
-                        <meta name="description" content={siteDescription} />
-                        <meta name="theme-color" content="#4C88B3"></meta>
-                </Helmet>
+        <div id="main">
+          <section id="one">
+            <header className="major">
+              <h2>Photographer based in Phnom Penh, Cambodia.</h2>
+              <Navbar></Navbar>
+            </header>
+            <p>
+              Welcome to my site! Below you can find some of my works.
+              <br></br>
+              Capturing photos of beautiful <Link to="/portraits">
+                people
+              </Link>{' '}
+              and beautiful <Link to="/landscapes">scenery</Link> have always
+              brought me joy.
+              <br></br>
+              Below, in no particular photo, are some of my favorite shots. Hope
+              you like them!
+            </p>
+            <ul className="actions">
+              {/*<li><a href="#" className="button">Learn More</a></li>*/}
+            </ul>
+          </section>
 
-                <div id="main">
+          <section id="two">
+            <h2 id="landscapes">Landscapes</h2>
+            <p>(Click to enlarge)</p>
+            <Gallery
+              images={DEFAULT_IMAGES.map(
+                ({ id, source, thumbnail, caption, description }) => ({
+                  source,
+                  thumbnail,
+                  caption,
+                  description,
+                })
+              )}
+            />
 
-                    <section id="one">
-                        <header className="major">
-                            <h2>Photographer based in Phnom Penh, Cambodia.</h2>
-                            <Navbar></Navbar>
-                        </header>
-                        <p>Welcome to my site! Below you can find some of my works.
-                            <br></br>
-                            Capturing photos of beautiful <Link to="/portraits">people</Link> and beautiful <Link to="/landscapes">scenery</Link> have always brought me joy.
-                            <br></br>
-                            Below, in no particular photo, are some of my favorite shots. Hope you like them!
-                        </p>
-                        <ul className="actions">
-                            {/*<li><a href="#" className="button">Learn More</a></li>*/}
-                        </ul>
-                    </section>
+            <ul className="actions">
+              <li>
+                <Link to="/landscapes" className="button">
+                  See More
+                </Link>
+              </li>
+            </ul>
 
-                    <section id="two">
-                        <h2 id="landscapes">Landscapes</h2>
-                        <p>(Click to enlarge)</p>
-                        <Gallery images={DEFAULT_IMAGES.map(({ id, source, thumbnail, caption, description }) => ({
-                            source,
-                            thumbnail,
-                            caption,
-                            description
-                        }))} />
+            {/* masaya code, portrait shots */}
+            <br></br>
+            <h2 id="portraits">Portraits</h2>
+            <p>(Click to enlarge)</p>
+            <Gallery
+              images={PORTRAIT_IMAGES.map(
+                ({ id, source, thumbnail, caption, description }) => ({
+                  source,
+                  thumbnail,
+                  caption,
+                  description,
+                })
+              )}
+            />
 
-                        <ul className="actions">
-                            <li><Link to="/landscapes" className="button">See More</Link></li>
-                        </ul>
+            <ul className="actions">
+              <li>
+                <Link to="/portraits" className="button">
+                  See More
+                </Link>
+              </li>
+            </ul>
+          </section>
 
-                        {/* masaya code, portrait shots */}
-                        <br></br>
-                        <h2 id="portraits">Portraits</h2>
-                        <p>(Click to enlarge)</p>
-                        <Gallery images={PORTRAIT_IMAGES.map(({ id, source, thumbnail, caption, description }) => ({
-                            source,
-                            thumbnail,
-                            caption,
-                            description
-                        }))} />
-
-                        <ul className="actions">    
-                            <li><Link to="/portraits" className="button">See More</Link></li>
-                        </ul>
-
-                    </section>
-
-                    <section id="three">
-                        <h2 id="contact">Get In Touch</h2>
-                        <p>Don't hesitate to leave me a message, we'll discuss ways to work together!</p>
-                        <div className="row">
-                            <div className="8u 12u$(small)">
-                                <MyForm>
-
-                                </MyForm>
-                            </div>
-                            <div className="4u 12u$(small)">
-                                <ul className="labeled-icons">
-                                    <li>
-                                        <h3 className="icon fa-home"><span className="label">Address</span></h3>
-                                        Phnom Penh<br />
-                                        Cambodia<br />
-                                    </li>
-                                    <li>
-                                        <h3 className="icon fa-mobile"><span className="label">Phone</span></h3>
-                                        +855 96 780 2296
-                                    </li>
-                                    <li>
-                                        <h3 className="icon fa-envelope-o"><span className="label">Email</span></h3>
-                                        <a href="mailto:masayashida@gmail.com" target="_blank" rel="noopener noreferrer">masayashida@gmail.com</a>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </section>
-
-                </div>
-
-            </Layout>
-        )
-    }
+          <section id="three">
+            <h2 id="contact">Get In Touch</h2>
+            <p>
+              Don't hesitate to leave me a message, we'll discuss ways to work
+              together!
+            </p>
+            <div className="row">
+              <div className="8u 12u$(small)">
+                <MyForm></MyForm>
+              </div>
+              <div className="4u 12u$(small)">
+                <ul className="labeled-icons">
+                  <li>
+                    <h3 className="icon fa-home">
+                      <span className="label">Address</span>
+                    </h3>
+                    Phnom Penh
+                    <br />
+                    Cambodia
+                    <br />
+                  </li>
+                  <li>
+                    <h3 className="icon fa-mobile">
+                      <span className="label">Phone</span>
+                    </h3>
+                    +855 96 780 2296
+                  </li>
+                  <li>
+                    <h3 className="icon fa-envelope-o">
+                      <span className="label">Email</span>
+                    </h3>
+                    <a
+                      href="mailto:masayashida@gmail.com"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      masayashida@gmail.com
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </section>
+        </div>
+      </Layout>
+    )
+  }
 }
 
 export default HomeIndex
